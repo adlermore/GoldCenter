5164f12af3fafecd06b4daab8617780ed45ce196
Ashot <52175898+adlermore@users.noreply.github.com>
2024-09-13 12:47:28 +0000

    Feat: Document Tasks Add

---
+++ documentTask.js
@@ -0,0 +1,27 @@
+// ADD FAVORITE 
+
+// Product Heart Click
+// if (IsAuth()){
+//   add heart cirlce Count
+//   Add Product Object in FavoriTes List (AddFavoriteSlice)
+// }else{
+//   Open Login Popup 
+// }
+
+// Menu Heart Click
+// if (IsAuth()){
+//   add heart cirlce Count
+//   Redirect to Account Page
+// }else{
+//   Open Login Popup 
+// }
+
+
+// Hide Popup Counstructor Layout in Popup Close Mode 
+// Change Search border Color 
+// Color Mode Css Classes add 
+// Product Listing Filter Clear function 
+// Add Collection Icon In product Block
+
+
+//Account Inner Pages Continune 
