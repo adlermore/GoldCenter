c03117743b24bf37104283d8279c393a1fd74af0
Ashot <ashten@logixsoftware.com>
2024-10-11 21:59:32 +0400

    local commit

--- app/brand/[slug]/page.jsx
+++ app/brand/[slug]/page.jsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import PageLoader from '@/components/PageLoader';
+import { JsonContext } from '@/context/jsonContext';
+import request from '@/utils/hooks/request';
+import React, { useContext, useEffect } from 'react'
 
 function BrandPage({ params }) {
+  
+  const { silverMode } = useContext(JsonContext);
+
+
+  // useEffect(()=>{
+  //   request(`${process.env.NEXT_PUBLIC_DATA_API}/catalog/top/${silverMode ? 'silver' : 'gold' }`)
+  //   .then((data) => {
+  //     setProductResponse(data);
+  //   })
+  //   request(`${process.env.NEXT_PUBLIC_DATA_API}/brands`)
+  //   .then((data) => {
+  //     setBrandsData(data.brands);
+  //   })
+  //   .catch(error => {
+  //       console.log(error);
+  //   })
+
+  // },[silverMode])
+
+
+  // if(!productResponse || !setBrandsData){
+  //   return <PageLoader />
+  // }
+
+
+
   console.log(params.slug);
+
   
   return (
     <div>BrandPage</div>
